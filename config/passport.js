348937b02382057b87a4f8312a7ca23440073528
kazuo <kazuking2215@gmail.com>
2025-05-18 04:06:54 +0900

    追加

---
+++ config/passport.js
@@ -0,0 +1,62 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const knex = require("../db/knex");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+module.exports = function(app){
+  app.use(session({
+	secret: "secretCuisine123",
+	resave: false,
+	saveUninitialized: false,
+	cookie:{
+		maxAge: 24*60*60*1000
+	}
+  }));
+	
+	
+	
+  passport.serializeUser(function(user,done){
+	done(null,user.id);
+  });
+
+  passport.deserializeUser(async function(id,done){
+	try{
+		const user = await User.findById(id);
+		done(null,user);
+	}catch(error){
+		done(error,null);
+	}
+  });
+
+  passport.use(new LocalStrategy({
+  	usernameField: "username",
+  	passwordField: "password",
+  },function(username,password,done){
+  	knex("users")
+		.where({
+          name: username,
+        })
+        .select("*")
+        .then(async function (results) {
+          if (results.length === 0) {
+            return done(null, false, {message: "Invalid User"});
+          } else if (await bcrypt.compare(password, results[0].password)) {
+            return done(null, results[0]);
+          } else {
+            return done(null, false, {message: "Invalid User"});
+          }
+        })
+        .catch(function (err) {
+          console.error(err);
+          return done(null, false, {message: err.toString()})
+        });
+    }
+  ));
+
+  app.use(flash());
+  app.use(passport.initialize());
+  app.use(passport.session());
+};
---
+++ routes/signup.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const knex = require("../db/knex");
+const bcrypt = require("bcrypt");
+
+router.get('/',function (req,res,next){
+	const isAuth = req.isAuthenticated();
+
+	res.render('signup',{
+		title: 'Sign up',
+		isAuth: isAuth,
+	});
+});
+
+router.post('/',function(req,res,next){
+	const isAuth = req.isAuthenticated();
+	const username = req.body.username;
+	const password = req.body.password;
+	const repassword = req.body.repassword;
+
+	knex("users")
+		.where({name: username})
+		.select("*")
+		.then(async function(result){ //async関数でawait使える
+			if(result.length !== 0){
+				res.render("signup",{
+					title: "Sign up",
+					isAuth: isAuth,
+					errorMessage: ["このユーザ名は既に使われています"],
+				})
+			}else if(password === repassword){
+				const hashedPassword = await bcrypt.hash(password, 10);
+		
+				knex("users")
+					.insert({name: username,password: hashedPassword})
+					.then(function(){
+						res.redirect("/");
+					})
+					.catch(function(err){
+						console.error(err);
+						res.render("signup",{
+							title: "Sign up",
+							isAuth: isAuth,
+							errorMessage: [err.sqlMessage],
+						});
+					});
+			}else{
+				res.render("signup",{
+					title: "Sign up",
+					isAuth: isAuth,
+					errorMessage: ["パスワードが一致しません"],
+					});
+				}
+			})
+		.catch(function(err){
+			console.error(err);
+			res.render("signup",{
+				title: "Sign up",
+				isAuth: isAuth,
+				errorMessage: [err.sqlMessage],
+			});
+		});
+});
+
+module.exports = router;
